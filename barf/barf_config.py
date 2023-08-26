d3ddd1fcc1b990e630cebc423aac6a0176bf7e70
maturk <matiasturkulainen@gmail.com>
2023-08-26 10:47:56 +0300

    add grad barf

--- barf/barf_config.py
+++ barf/barf_config.py
@@ -17,7 +17,7 @@ from nerfstudio.pipelines.base_pipeline import VanillaPipelineConfig
 from nerfstudio.plugins.types import MethodSpecification
 
 from barf.barf_pipeline import BARFPipelineConfig
-from barf.barf import BARFFreqModelConfig, BARFHashModelConfig
+from barf.barf import BARFFreqModelConfig, BARFHashModelConfig, BARFGradientHashModelConfig
 
 max_num_iterations = 200000
 
@@ -57,7 +57,7 @@ barf_freq_method = MethodSpecification(
         viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
         vis="viewer",
     ),
-    description="BARF c2f freq encoding implementation.",
+    description="Vanilla BARF c2f freq encoding implementation.",
 )
 
 barf_hash_method = MethodSpecification(
@@ -99,5 +99,48 @@ barf_hash_method = MethodSpecification(
         viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
         vis="viewer",
     ),
-    description="BARF c2f hash encoding implementation.",
+    description="BARF c2f hash grid encoding implementation.",
+)
+
+
+barf_grad_hash_method = MethodSpecification(
+    config=TrainerConfig(
+        method_name="barf-grad",
+        steps_per_eval_batch=500,
+        steps_per_save=2000,
+        max_num_iterations=30000,
+        mixed_precision=True,
+        pipeline=BARFPipelineConfig(
+            datamanager=VanillaDataManagerConfig(
+                dataparser=NerfstudioDataParserConfig(),
+                train_num_rays_per_batch=4096,
+                eval_num_rays_per_batch=4096,
+                camera_optimizer=CameraOptimizerConfig(  # Blender synthetic data
+                    mode="SO3xR3",
+                    optimizer=AdamOptimizerConfig(lr=6e-4, eps=1e-8, weight_decay=1e-2),
+                    scheduler=ExponentialDecaySchedulerConfig(lr_final=6e-6, max_steps=200000),
+                ),
+            ),
+            model=BARFGradientHashModelConfig(
+                eval_num_rays_per_chunk=1 << 15,
+            ),
+        ),
+        optimizers={
+            "proposal_networks": {
+                "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
+                "scheduler": ExponentialDecaySchedulerConfig(lr_final=0.0001, max_steps=200000),
+            },
+            "fields": {
+                "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
+                "scheduler": ExponentialDecaySchedulerConfig(lr_final=0.0001, max_steps=200000),
+            },
+            # "camera_opt": {
+            #     "optimizer": AdamOptimizerConfig(lr=1e-3, eps=1e-15),
+            #     "scheduler": ExponentialDecaySchedulerConfig(lr_final=1e-4, max_steps=5000),
+            # },
+        },
+        viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
+        vis="viewer",
+    ),
+    description="BARF gradient scaled hash encoding implementation.",
 )
